f76480e6bfacbeb25a8bc2d67fb70a138b8aea54
Jesse <nyemiteijesse@gmail.com>
2022-09-14 08:04:56 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#Include <stdio.h>
+
+/**
+ * print_alphabet - print alphabet in lower case
+ */ 
+void print_alphabet(void)
+{
+for(int alph = 'a'; alph <= 'z'; alph++)
+{
+  putchar(alph);
+}
+}
+ 
