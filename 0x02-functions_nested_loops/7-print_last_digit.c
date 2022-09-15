5fca93afeba0cfe127716e5a64f5f66be7f77ad8
Jesse <nyemiteijesse@gmail.com>
2022-09-15 04:15:30 -0700

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ *
+ * @n: digit
+ *
+ * Return: 0 for success
+ */
+int print_last_digit(int n)
+{
+int rem = n % 10;
+return (rem);
+}
