8d500eb9851b3d6212fb075cd99409928e8d36a1
Jesse <nyemiteijesse@gmail.com>
2022-09-16 02:23:36 -0700

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - tests positive or negative function
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+int i;
+i = 98;
+positive_or_negative(i);
+return (0);
+}
