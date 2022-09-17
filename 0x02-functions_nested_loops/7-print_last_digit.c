4c7528ea2d55c3ad1536bfd5cbeb47c76fa691d2
Jesse <nyemiteijesse@gmail.com>
2022-09-17 00:44:04 -0700

    10

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+0;10;1c
 /**
  * print_last_digit - print last digit
  *
@@ -19,7 +19,7 @@ return (-rem);
 else
 {
 rem = n % 10;
-_putchar('0' + rem); 
+ _putchar('0' + rem);
 return (rem);
 }
 }
