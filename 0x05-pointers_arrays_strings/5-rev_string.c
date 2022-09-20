3a30c13a04775c1bcd0438d5297ed4ff4520c6a0
Jesse <nyemiteijesse@gmail.com>
2022-09-19 23:09:33 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: pointer
+ */
+void rev_string(char *s)
+{
+int i = 0;
+while(s[length] > '\0')
+{
+length++;
+}
+for (i = length - 1; length >= 0; length++)
+{
+_putchar(s[i]);
+}
+}
