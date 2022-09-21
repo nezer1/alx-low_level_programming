274ad80b31deb435754150d70c9494971942ca33
Jesse <nyemiteijesse@gmail.com>
2022-09-20 23:32:10 -0700

    5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,34 @@ void rev_string(char *s)
 {
 int length = 0;
 int i = 0;
+length = string_len(s);
+char st[length];
+
+for (i = length - 1; i >= 0; i--)
+{
+length;
+st[i - (length - 1)] = s[i];
+}
+while(i < length)
+{
+s[i] = st[i];
+i++;
+}
+}
+
+/**
+ * string_len - length of string
+ * @s: pointer
+ * Return: length 
+ */
+void stirng_len(char *s)
+{
+int length = 0;
+int i = 0;
 int len = 0;
 while (s[length] > '\0')
 {
 length++;
 }
-for (i = length - 1; i >= 0; i--)
-{
-len = length;
-s[i - (len - 1)] = s[i];
-}
+return (length);
 }
