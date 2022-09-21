bf42917a10e958b9cc83f08d2ece7639da206efc
Jesse <nyemiteijesse@gmail.com>
2022-09-21 03:00:32 -0700

    7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * put_half - print half of a string 
+ * @str: string
+ */
+void puts_half(char *str)
+{
+length = string_len(str);
+int i = 0;
+int j = 0;
+int len = 0;
+while (i < length) 
+{
+len = length;
+if (length % 2 == 0)
+{
+while (j < length/2)
+{
+_putchar(str[len--]);
+j++;
+}
+}
+else
+{
+while (j < (length - 1) / 2)
+{
+_putchar(str[len--]);
+}
+}
+_putchar('\n');
+}
+}
+
+/**
+ * string_len - string length
+ * @s: pointer
+ */
+int string_len(char *s)
+{
+int length = 0;
+while (s[length] > '\0')
+{
+length++;
+}
+return (length);
+}
+  
