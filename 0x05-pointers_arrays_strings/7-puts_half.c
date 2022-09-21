70cd54f904bfb2f7e011f93782d05692158dc14f
Jesse <nyemiteijesse@gmail.com>
2022-09-21 03:03:29 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,16 +1,16 @@
 #include "main.h"
-
+0;10;1c
 /**
  * put_half - print half of a string 
  * @str: string
  */
 void puts_half(char *str)
 {
-length = string_len(str);
 int i = 0;
 int j = 0;
 int len = 0;
 int length = 0;
+length = string_len(str);
 while (i < length) 
 {
 len = length;
