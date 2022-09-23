6d62a556053cfed964de91594e84c27b8f1b0c5d
Jesse <nyemiteijesse@gmail.com>
2022-09-23 04:35:46 -0700

    3

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers- print
+ */
+void print_most_numbers(void)
+{
+int i = 48;
+while (i < 57)
+{
+if (i != 50 || i != 52)
+{
+_putchar(i);
+i++;
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,5 @@ void _putchar(int);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+void print_most_numbers(void);
 #endif /* functions */
