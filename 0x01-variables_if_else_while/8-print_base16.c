bc21563b7dacb2fde077fa66d7a627194dc7aa25
Jesse <nyemiteijesse@gmail.com>
2022-09-23 14:04:00 -0700

    1

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main- print
+ * Return: 0;
+ */
+int main(void)
+{
+int i = 48;
+while (i <= 'f')
+{
+if (i <= '9')
+{
+putchar(i);
+}
+else
+{
+i = 'a';
+putchar(i);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
