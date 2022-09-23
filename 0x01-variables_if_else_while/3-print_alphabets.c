87f6e27c9afac09b48fdaf5e3e950551134f3af6
Jesse <nyemiteijesse@gmail.com>
2022-09-23 08:57:02 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 int i = 0;
-for (i = 'A'; i <= 'Z'; i++;)
+for (i = 'a'; i <= 'z'; i++)
 {
 putchar(i);
 }
-for (i = 'a'; i <= 'z'; i++;)
+for (i = 'A'; i <= 'Z'; i++)
 {
 putchar(i);
 }
