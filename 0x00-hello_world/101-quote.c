72f503a9463a19833b574da3d3f7e6a4d72d8194
Jesse <nyemiteijesse@gmail.com>
2022-09-24 05:40:57 -0700

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-char r[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-fprintf(stderr,r);
+char *r= "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+fprintf(stderr,*r);
 return (0);
 }
