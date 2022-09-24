41f4ae914978e4c1c5ed53efecf6b4e79e1b63e3
Jesse <nyemiteijesse@gmail.com>
2022-09-24 06:01:11 -0700

    1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+0;10;1c
 /**
  * main- stderr
  * Return:0
@@ -7,5 +7,5 @@
 int main(void)
 {
 fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (0);
+return (1);
 }
