6a90f4b0b83c4cc6b3e2f657967ffdc98602c7c8
Jesse <nyemiteijesse@gmail.com>
2022-09-25 10:34:19 -0700

    1

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main- print fizzbuzz
+ * Return: int
+ */
+int main(void)
+{
+int i = 1;
+while (i <= 100)
+{
+if (i % 3 == 0 && i % 5 != 0)
+{
+printf("Fizz");
+}
+else if (i % 5 == 0 && i % 3 != 0)
+{
+printf("Buzz");
+}
+else if (i % 5 == 0 && i % 3 == 0)
+{
+printf("FizzBuzz");
+}
+else
+{
+printf("%d",i);
+}
+i++;
+putchar(' ');
+}
+return (0);
+}
