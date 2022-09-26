9022fe6fed2286e83392b9b7da72e42be2940b20
Jesse <nyemiteijesse@gmail.com>
2022-09-26 14:32:19 -0700

    0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memcpy- copy memory
+ * @dest:dest pointer
+ * @src: src pointer
+ * @n: number limit
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i = 0;
+char *temp;
+temp = dest;
+while (i < n)
+{
+dest[i++] = src[i++];
+}
+return (temp);
+}
