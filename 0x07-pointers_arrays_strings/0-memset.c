1633527f0581c990436f2ddfb9314efa740a808f
Jesse <nyemiteijesse@gmail.com>
2022-09-26 14:19:52 -0700

    0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
  * _memset- set memory
+ * @s: pointer
+ * @b: character
+ * @n: number limit
+ * Return: pointer to s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
